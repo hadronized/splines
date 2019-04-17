48623701a7402881c694ffcc669dda142483d475
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-04-17 14:16:31 +0200

    Fix some documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -154,19 +154,19 @@ impl<T, V> Key<T, V> {
 #[cfg_attr(feature = "serialization", derive(Deserialize, Serialize))]
 #[cfg_attr(feature = "serialization", serde(rename_all = "snake_case"))]
 pub enum Interpolation<T> {
-  /// Hold a [`Key`] until the time passes the normalized step threshold, in which case the next
-  /// key is used.
+  /// Hold a [`Key`] until the interpolator value passes the normalized step threshold, in which
+  /// case the next key is used.
   ///
-  /// *Note: if you set the threshold to `0.5`, the first key will be used until the time is half
-  /// between the two keys; the second key will be in used afterwards. If you set it to `1.0`, the
-  /// first key will be kept until the next key. Set it to `0.` and the first key will never be
-  /// used.*
+  /// > Note: if you set the threshold to `0.5`, the first key will be used until half the time
+  /// > between the two keys; the second key will be in used afterwards. If you set it to `1.0`, the
+  /// > first key will be kept until the next key. Set it to `0.` and the first key will never be
+  /// > used.
   Step(T),
   /// Linear interpolation between a key and the next one.
   Linear,
   /// Cosine interpolation between a key and the next one.
   Cosine,
-  /// Catmull-Rom interpolation.
+  /// Catmull-Rom interpolation, performing a cubic Hermite interpolation using four keys.
   CatmullRom
 }
 
